3354e4bbaaef0fb3877d4bcd1c5d19e4cfe4a489
Pedro <estudiosia12@gmail.com>
2025-10-22 20:04:57 -0500

    Actualización del Model: agregados getters and setters

--- src/main/java/com/example/ProyectoGym/Model/Miembro.java
+++ src/main/java/com/example/ProyectoGym/Model/Miembro.java
@@ -29,4 +29,85 @@ public class Miembro {
         this.fechaRegistro = LocalDate.now();
         this.activo = true;
     }
+
+    public Long getId_miembro() {
+        return id_miembro;
+    }
+
+    public void setId_miembro(Long id_miembro) {
+        this.id_miembro = id_miembro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
 }
